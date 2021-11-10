dd68377521ecc240df44ded4e9beb2fb6b13269f
LauraC08 <laura-chenais@hotmail.fr>
2021-11-10 16:07:16 +0100

    mise en place + signup fonctionnalité

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor() {
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+      apiKey: "AIzaSyAihcP5K6ihw-ax6hsO-BrpFh9ZL9EZBog",
+      authDomain: "bookshelves-b8645.firebaseapp.com",
+      projectId: "bookshelves-b8645",
+      storageBucket: "bookshelves-b8645.appspot.com",
+      messagingSenderId: "971746993381",
+      appId: "1:971746993381:web:0747dbb51fca7d0161600c",
+      measurementId: "G-D8S3QMB5TQ"
+    };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+  }
 }
