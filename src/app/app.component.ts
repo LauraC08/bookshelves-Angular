59fd56136d1faf4000ca3483e32b8ae3c9dc0e71
LauraC08 <laura-chenais@hotmail.fr>
2021-11-11 23:22:20 +0100

    book service + fonctionnalittés associées

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,12 @@ export class AppComponent {
     const firebaseConfig = {
       apiKey: "AIzaSyAihcP5K6ihw-ax6hsO-BrpFh9ZL9EZBog",
       authDomain: "bookshelves-b8645.firebaseapp.com",
+      databaseURL: "https://bookshelves-b8645-default-rtdb.europe-west1.firebasedatabase.app",
       projectId: "bookshelves-b8645",
       storageBucket: "bookshelves-b8645.appspot.com",
       messagingSenderId: "971746993381",
-      appId: "1:971746993381:web:0747dbb51fca7d0161600c",
-      measurementId: "G-D8S3QMB5TQ"
+      appId: "1:971746993381:web:a1af623e58f34d0561600c",
+      measurementId: "G-SEC5HE47L5"
     };
     
     // Initialize Firebase
--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,9 +1,70 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Book } from '../models/Book.model';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class BooksService {
 
-  constructor() { }
+  constructor() { 
+    this.getBooks();
+  }
+
+  books: Book[] = [];
+  booksSubject = new Subject<Book[]>();
+  errorMessage!: string;
+
+  emitBooks() {
+    this.booksSubject.next(this.books);
+  }
+
+  saveBooks() {
+    firebase.database().ref('/books').set(this.books);
+}
+
+  getBooks() {
+    firebase.database().ref('/books')
+      .on('value', (data) => {
+          this.books = data.val() ? data.val() : [];
+          this.emitBooks();
+        }
+      );
+  }
+
+  getSingleBook(id: number) {
+    return new Promise<Book>(
+      (resolve, reject) => {
+        firebase.database().ref('/books/' + id).once('value').then(
+          (data) => {
+            resolve(data.val());
+          }, (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  createNewBook(newBook: Book) {
+    this.books.push(newBook);
+    this.saveBooks();
+    this.emitBooks();
+  }
+
+  removeBook(book: Book) {
+    const bookIndexToRemove = this.books.findIndex(
+      (bookEl) => {
+        if(bookEl === book) {
+          return true;
+        } else {
+          return this.errorMessage;
+        }
+      }
+    );
+    this.books.splice(bookIndexToRemove, 1);
+    this.saveBooks();
+    this.emitBooks();
+  }
+
 }
